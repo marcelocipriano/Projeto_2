feb80fa0dd8d13ee794a1792db19a44b53432188
marcelocipriano <marcelocipriano@rocketmail.com>
2017-04-23 12:57:50 -0300

    Novo CRUD incluido
    Incluido função de cadastrar e listar usuarios

---
+++ usuarios/incluir_usuario.php
@@ -0,0 +1,27 @@
+<?php
+include('../menu/index.tpl.php');
+?>
+<html>
+	<head>
+	<title>Loja Virtual</title>
+	<link rel="stylesheet" type="text/css" href="estilo_menu.css"/>
+	<meta charset="ISO-8859-1"/>
+	</head>
+	<body>
+		<form method="post" action="listar_usu.php">
+			Nome: <input type="text" name="nome"><br><br>
+			E-mail: <input type="email" name="email"><br><br>
+			Senha: <input type="password" name="senha"><br><br>
+			Perfil: <select name="perfil">
+						<option value="A">
+							Administrador
+						</option>
+						<option value="C">
+							Cliente
+						</option>
+					</select><br><br>
+			Ativo: <input type="radio" name="ativo" checked><br><br>
+			<input type="submit" value="Gravar" name="btnGravar">
+		</form>
+	</body>
+</html>
--- usuarios/listar_usu.php
+++ usuarios/listar_usu.php
@@ -1,20 +1,125 @@
+<table width="100%" border="1">
+	<tr>
+		<td bgcolor="gray">ID Usuario</td>
+		<td bgcolor="gray">Login</td>
+		<td bgcolor="gray">Senha</td>
+		<td bgcolor="gray">Nome</td>
+		<td bgcolor="gray">Editar</td>
+		<td bgcolor="gray">Apagar</td>
+		<td bgcolor="gray"><a href='incluir_usuario.php'>Gravar</a></td>
+	</tr>
+
 <?php
 
 include('../auth/controle_de_acesso.php');
 include('../db/index.php');
 
-$q = odbc_exec($db, 'select * from usuario');
-while($r = odbc_fetch_array($q){
+$query = odbc_exec($db,' SELECT * FROM Usuario');
+
+//$result = odbc_fetch_array($query);
 	
-	echo $r;
+//	echo "Nome: {$result['nomeUsuario']}<br>";
 	
+	
+	while ($result = odbc_fetch_array($query)){
+		
+		echo " <tr>
+				<td>{$result['idUsuario']}</td>
+				<td>{$result['loginUsuario']}</td>
+				<td>{$result['senhaUsuario']}</td>
+				<td>{$result['nomeUsuario']}</td>
+				<td><a href='?acao=excluir&id={$result['idUsuario']}'>Excluir</a></td>
+			</tr>";
+	}
+	
+
+
+if(isset($_REQUEST['acao'])){
+	
+	$acao = $_REQUEST['acao'];
+	
+	switch($acao){
+		case 'incluir':
+		
+			break;
+		
+		case 'excluir':
+			if(is_numeric($_GET['id'])){
+				if($result = odbc_exec($db, "DELETE FROM Usuario WHERE IdUsuario = {$_GET['id']}")){
+					if(odbc_num_rows($result) > 0){
+					$msg = "Usuario excluido com sucesso";
+					}
+				}else{
+						$erro = "Erro ao excluir o usuario";
+				}
+			}else{
+				$erro = "ID Invalido";
+			}
+			break;
+		default:
+			$query = odbc_exec($db,' SELECT * FROM Usuario');
+
+//$result = odbc_fetch_array($query);
+	
+//	echo "Nome: {$result['nomeUsuario']}<br>";
+	
+	
+	while ($result = odbc_fetch_array($query)){
+		
+		echo " <tr>
+				<td>{$result['idUsuario']}</td>
+				<td>{$result['loginUsuario']}</td>
+				<td>{$result['senhaUsuario']}</td>
+				<td>{$result['nomeUsuario']}</td>
+				<td><a href='?acao=excluir&id={$result['idUsuario']}'>Excluir</a></td>
+			</tr>";
+	}
+		
+	}
+	
+}else{
+	if(isset($_POST['btnGravar'])){
+		
+		//Trata nome
+		$nome = preg_replace("/[^a-z A-Z 0-9]+/", "", $_POST['nome']);
+		
+		//Trata email
+		$email_exploded = explode();
+		$email = preg_replace("/[^a-z A-Z 0-9]+/", "", $_POST['email']);
+		
+		//Trata senha
+		$senha = str_replace("/[^a-z A-Z 0-9]+/", "", $_POST['nome']);
+		$senha = str_replace("/[^a-z A-Z 0-9]+/", "", $_POST['nome']);
+		$senha = str_replace("/[^a-z A-Z 0-9]+/", "", $_POST['nome']);
+		
+		//Tratar Perfil
+		$perfil = $_POST['Perfil'] != 'A' && $_POST['Perfil'] != 'C' ? 'C': $_POST['Perfil'];
+		
+		//Tratar Ativo, se colocado qualquer valor é convertido em booleano: verdadeiro ou falso
+		$_POST $ativo
+		$ativo = (bool) $_POST['ativo'];
+		$ativo = $ativo === true ? 1 : 0;
+		
+		if(odbc_exec($db, "INSERT INTO Usuario
+							(LoginUsuario,
+							senhaUsusario,
+							nomeUsuario,
+							tipoPerfil,
+							usuarioAtivo)
+							VALUES
+							('$email',
+							HASHBYTES('SHA1', ''),
+							'$nome',
+							'$perfil',
+							$ativo)
+							")){
+			$msg = "Usuario Gravado com sucesso";
+		}else{
+			$erro = "Erro ao gravar usuario";
+		}
+		
+	}
 }
 
-
-include('index.tpl.php');
-
-
-
-
-
 ?>
+</table>
